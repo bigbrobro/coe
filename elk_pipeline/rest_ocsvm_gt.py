41bd568bf659e0748e084a1038f8b6e532f50cb0
gamzattirev <fmariko1008@gmail.com>
2018-12-20 20:11:50 +0900

    add sample programs for ELK pileline

--- elk_pipeline/rest_ocsvm_gt.py
+++ elk_pipeline/rest_ocsvm_gt.py
@@ -1,12 +1,7 @@
-from sklearn.externals import joblib
-import pandas as pd
-import numpy as np
 import urllib.parse
 from flask import Flask, jsonify, request
-from machine_learning import ML
 from signature_detection import SignatureDetector
 import InputLog
-import send_alert
 import pickle
 import os
 
@@ -21,31 +16,30 @@ class MyFlask (Flask):
                        static_folder, template_folder,
                        instance_path, instance_relative_config)
 
-        print('init called')
+        print('Initialize Flask')
         if os.path.exists(log)==True:
             with open(log, mode='rb') as f:
                 SignatureDetector.df=pickle.load(f)
+
+        try:
+            file_admin = open("./admin.csv")
+            SignatureDetector.adminlist = file_admin.readlines()
+            file_cmd = open("./command.csv")
+            SignatureDetector.cmdlist = file_cmd.readlines()
+        except Exception as e:
+            print(e)
+        finally:
+            file_admin.close()
+            file_cmd.close()
+
+        SignatureDetector.adminlist = [s.replace('\n', '') for s in SignatureDetector.adminlist]
+        SignatureDetector.cmdlist = [s.replace('\n', '') for s in SignatureDetector.cmdlist]
 
     def __del__(self):
         print('destructor called')
 
 
 app = MyFlask(__name__)
-
-clf_4674 = joblib.load('ocsvm_gt_4674.pkl')
-base_dummies_4674 = pd.read_csv('data_dummies_4674.csv')
-clf_4688 = joblib.load('ocsvm_gt_4688.pkl')
-base_dummies_4688 = pd.read_csv('data_dummies_4688.csv')
-
-SignatureDetector.df_admin = pd.read_csv("./admin.csv")
-SignatureDetector.df_cmd = pd.read_csv("./command.csv")
-
-
-# If you run this code on the other computer, you might need to remove commentout below.
-# Sometimes mode.predict function does not load correctly.
-# import numpy as ap
-# X = np.zeros((10, max_len))
-# model.predict(X, batch_size=32)
 
 @app.route('/preds', methods=['POST'])
 def preds():
@@ -84,21 +78,11 @@ def preds():
         sharedname = sharedname.strip("'")
         sharedname = sharedname.lower()
 
-    # To specify parameter as Object
     inputLog = InputLog.InputLog(datetime, eventid, accountname, clientaddr, servicename, processname, objectname, sharedname)
-    # update start by gam
     result = SignatureDetector.signature_detect(inputLog)
-
-    # update end
-    clientaddr = inputLog.get_clientaddr()
-    processname=inputLog.get_processname()
 
     print(inputLog.get_eventid()+","+inputLog.get_accountname()+","+inputLog.get_clientaddr()+","+inputLog.get_processname())
 
-    if (result == SignatureDetector.RESULT_CMD or result == SignatureDetector.RESULT_MAL_CMD):
-        result = ML.preds(eventid, accountname, processname, objectname, base_dummies_4674, clf_4674, base_dummies_4688, clf_4688)
-    if (result != SignatureDetector.RESULT_NORMAL and result != ML.RESULT_WARN):
-        send_alert.Send_alert(result, datetime, eventid, accountname, clientaddr, servicename, processname, objectname, sharedname)
 
     return result
 
@@ -106,6 +90,5 @@ if __name__ == '__main__':
     try:
         app.run(host='0.0.0.0')
     finally:
-        print('finally called')
         with open(log, mode='wb') as handle:
             pickle.dump(SignatureDetector.df, handle, protocol=pickle.HIGHEST_PROTOCOL)
--- elk_pipeline/signature_detection.py
+++ elk_pipeline/signature_detection.py
@@ -6,16 +6,16 @@ import InputLog
 class SignatureDetector:
     EVENT_PRIV = "4672"
     EVENT_PROCESS = "4688"
-    SYSTEM_DIR = "c:\windows";
-    SYSTEM_DIR2 = "c:\program files";
+    SYSTEM_DIR = ["c:\windows","c:\program files"];
     RESULT_NORMAL="normal"
     RESULT_PRIV = "Unexpected privilege is used"
     RESULT_CMD="Command on blackList is used"
     RESULT_MAL_CMD = "Abnormal command or tool is used"
 
-    df=pd.DataFrame(data=None, index=None, columns=["datetime","eventid","accountname","clientaddr","servicename","processname","objectname"], dtype=None, copy=False)
-    df_admin = pd.DataFrame(data=None, index=None, columns=[ "accountname"], dtype=None, copy=False)
     df_cmd = pd.DataFrame(data=None, index=None, columns=["processname"], dtype=None, copy=False)
+
+    adminlist=[]
+    cmdlist=[]
 
 
     def __init__(self):
@@ -51,15 +51,12 @@ class SignatureDetector:
         elif (inputLog.get_eventid() == SignatureDetector.EVENT_PROCESS):
             result = SignatureDetector.isSuspiciousProcess(inputLog)
 
-        series = pd.Series([inputLog.get_datetime(),inputLog.get_eventid(),inputLog.get_accountname(),inputLog.get_clientaddr(),
-                      inputLog.get_servicename(),inputLog.get_processname(),inputLog.get_objectname(),inputLog.get_sharedname()], index=SignatureDetector.df.columns)
-        SignatureDetector.df=SignatureDetector.df.append(series, ignore_index = True)
 
         return result
 
     @staticmethod
     def isNotAdmin(inputLog):
-        logs = SignatureDetector.df_admin[(SignatureDetector.df_admin.accountname == inputLog.get_accountname())]
+        logs = [s for s in adminlist if s == inputLog.get_accountname()]
         if len(logs) == 0:
             return SignatureDetector.RESULT_PRIV
         else:
@@ -67,37 +64,45 @@ class SignatureDetector:
 
     @staticmethod
     def isSuspiciousProcess(inputLog):
-        if (inputLog.get_processname().find(SignatureDetector.SYSTEM_DIR)==-1 and inputLog.get_processname().find(SignatureDetector.SYSTEM_DIR2)==-1):
+        matched_list = [s for s in SignatureDetector.SYSTEM_DIR if s in inputLog.get_processname()]
+        if (len(matched_list)==0):
             return SignatureDetector.RESULT_MAL_CMD
         cmds=inputLog.get_processname().split("\\")
         cmd=cmds[len(cmds)-1]
-        logs = SignatureDetector.df_cmd[SignatureDetector.df_cmd.processname.str.contains(cmd)]
+        logs = [s for s in cmdlist if s==cmd]
         if len(logs)>0:
             return SignatureDetector.RESULT_CMD
 
         return SignatureDetector.RESULT_NORMAL
 
 
-SignatureDetector.df = pd.read_csv("./logs.csv")
-
-SignatureDetector.df_admin = pd.read_csv("./admin.csv")
-
-SignatureDetector.df_cmd = pd.read_csv("./command.csv")
-
-
-csv_file = io.open("./log.csv", mode="r", encoding="utf-8")
-f = csv.DictReader(csv_file, delimiter=",", doublequote=True, lineterminator="\r\n", quotechar='"', skipinitialspace=True)
-for row in f:
-    datetime=row.get("datetime")
-    eventid=row.get("eventid")
-    accountname=row.get("accountname").lower()
-    clientaddr=row.get("clientaddr")
-    servicename=row.get("servicename").lower()
-    processname=row.get("processname").lower()
-    objectname=row.get("objectname").lower()
-    sharedname = row.get("sharedname").lower()
-
-    # To specify parameter as Object
-    inputLog = InputLog.InputLog(datetime, eventid, accountname, clientaddr, servicename, processname, objectname,sharedname)
-    print(SignatureDetector.signature_detect(inputLog))
-
+# try:
+#     file_admin = open("./admin.csv")
+#     adminlist = file_admin.readlines()
+#     file_cmd = open("./command.csv")
+#     cmdlist = file_cmd.readlines()
+# except Exception as e:
+#     print(e)
+# finally:
+#     file_admin.close()
+#     file_cmd.close()
+#
+# adminlist = [s.replace('\n','') for s in adminlist]
+# cmdlist = [s.replace('\n','') for s in cmdlist]
+#
+# csv_file = io.open("./log.csv", mode="r", encoding="utf-8")
+# f = csv.DictReader(csv_file, delimiter=",", doublequote=True, lineterminator="\r\n", quotechar='"', skipinitialspace=True)
+# for row in f:
+#     datetime=row.get("datetime")
+#     eventid=row.get("eventid")
+#     accountname=row.get("accountname").lower()
+#     clientaddr=row.get("clientaddr")
+#     servicename=row.get("servicename").lower()
+#     processname=row.get("processname").lower()
+#     objectname=row.get("objectname").lower()
+#     sharedname = row.get("sharedname").lower()
+#
+#     # To specify parameter as Object
+#     inputLog = InputLog.InputLog(datetime, eventid, accountname, clientaddr, servicename, processname, objectname,sharedname)
+#     print(SignatureDetector.signature_detect(inputLog))
+#
